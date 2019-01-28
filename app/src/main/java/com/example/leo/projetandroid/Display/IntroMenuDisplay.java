37280c87907174b268481857ea893c3a26ff40be
lboisson <boisson.leo@gmail.com>
2019-01-28 19:33:07 +0100

    appli ouvre sur intromenu

--- app/src/main/java/com/example/leo/projetandroid/Display/IntroMenuDisplay.java
+++ app/src/main/java/com/example/leo/projetandroid/Display/IntroMenuDisplay.java
@@ -1,4 +1,15 @@
 package com.example.leo.projetandroid.Display;
 
-public class IntroMenuDisplay {
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.leo.projetandroid.R;
+
+public class IntroMenuDisplay extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intromenu_display);
+    }
 }
