62cdb2c2fbd7e688576ac24d277aa6b10f9d0837
lboisson <boisson.leo@gmail.com>
2019-01-28 20:13:25 +0100

    modif manifest

--- app/src/main/java/com/example/leo/projetandroid/Display/IntroMenuDisplay.java
+++ app/src/main/java/com/example/leo/projetandroid/Display/IntroMenuDisplay.java
@@ -1,7 +1,10 @@
 package com.example.leo.projetandroid.Display;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.leo.projetandroid.R;
 
@@ -11,5 +14,15 @@ public class IntroMenuDisplay extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intromenu_display);
+    }
+
+    public void ActivityToGameDisplay(View v){
+        Intent intent = new Intent(this, GameDisplay.class);
+        startActivity(intent);
+    }
+
+    public void ActivityToOption(View v){
+        Intent intent = new Intent(this, OptionDisplay.class);
+        startActivity(intent);
     }
 }
