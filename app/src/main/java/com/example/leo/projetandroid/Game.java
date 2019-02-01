718275f32b52b588fa905ae3155c77f40a646ce4
lboisson <boisson.leo@gmail.com>
2019-02-01 14:33:37 +0100

    constructeur

--- app/src/main/java/com/example/leo/projetandroid/Game.java
+++ app/src/main/java/com/example/leo/projetandroid/Game.java
@@ -2,13 +2,19 @@ package com.example.leo.projetandroid;
 
 public class Game {
 
-    //used for the singleton implementation
-    private static Game instance;
-    private Game(){ }
-
     private Inventory inventory;
     private Character character;
     private Map map;
+
+    //used for the singleton implementation
+    private static Game instance;
+    private Game(){
+        this.character = Character.getInstance();
+        this.inventory = Inventory.getInstance();
+        this.map = Map.getInstance();
+    }
+
+
 
     /**
      * Usage of the singleton pattern for the Game class
