3ea13f9d4069efbc2256ba02a06a864998f549fb
lboisson <boisson.leo@gmail.com>
2019-02-01 14:33:56 +0100

    constructeur

--- app/src/main/java/com/example/leo/projetandroid/Room.java
+++ app/src/main/java/com/example/leo/projetandroid/Room.java
@@ -1,4 +1,18 @@
 package com.example.leo.projetandroid;
 
 public class Room {
+
+    private int latitude;
+    private int longitude;
+
+    /**
+     * constructor of the Room class
+     */
+    public Room(int latitude, int longitude){
+        //TODO : random assignement of sprites for the floor, walls, doors
+
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
 }
