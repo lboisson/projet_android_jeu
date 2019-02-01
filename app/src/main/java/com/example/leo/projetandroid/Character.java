6c0daf99c5faf27d952eb2d614151d00c685f212
lboisson <boisson.leo@gmail.com>
2019-02-01 14:32:52 +0100

    constructeur

--- app/src/main/java/com/example/leo/projetandroid/Character.java
+++ app/src/main/java/com/example/leo/projetandroid/Character.java
@@ -1,4 +1,22 @@
 package com.example.leo.projetandroid;
 
-public class Character { // JESUISPASSELA
+public class Character {
+
+    //used for the singleton implementation
+    private static Character instance;
+    private Character(){
+
+    }
+
+    /**
+     * Usage of the singleton pattern for the Game class
+     * @return the unique instance of the game
+     */
+    public static synchronized Character getInstance(){
+        if(instance == null){
+            instance = new Character();
+        }
+        return instance;
+    }
+
 }
