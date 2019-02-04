838be5904ddb7d1caa5dd00f4ac374065a2e2d1f
lboisson <boisson.leo@gmail.com>
2019-02-04 18:03:56 +0100

    random sprites

--- app/src/main/java/com/example/leo/projetandroid/Room.java
+++ app/src/main/java/com/example/leo/projetandroid/Room.java
@@ -1,11 +1,15 @@
 package com.example.leo.projetandroid;
 
+import java.util.Random;
+
 public class Room {
+
+    //maybe change this, the number of different sprites existing
+    private int numberOfSprites = 2;
 
     // the coordinates of the room (used for the map_display)
     private int latitude;
     private int longitude;
-
 
     // the name of the sprites used for this particular room
     private String floor, wall_east, wall_west, wall_north, wall_south;
@@ -18,12 +22,35 @@ public class Room {
         this.longitude = longitude;
 
         //TODO : random assignement of sprites for the floor, walls, doors
-        this.floor = "floor";
-        this.wall_east = "wall_east_2";
-        this.wall_west = "wall_west";
-        this.wall_north = "wall_north";
-        this.wall_south = "wall_south";
+        this.floor = generateNameRandomSprite("floor");
+        this.wall_east = generateNameRandomSprite("wall_east");
+        this.wall_west = generateNameRandomSprite("wall_west");
+        this.wall_north = generateNameRandomSprite("wall_north");
+        this.wall_south = generateNameRandomSprite("wall_south");
     }
+
+
+    /**
+     *
+     * @param spriteName
+     * @return
+     */
+    private String generateNameRandomSprite(String spriteName){
+        final int randomNumber = new Random().nextInt((numberOfSprites - 1) + 1) + 1;
+        if(randomNumber<10){
+            return spriteName + "_00" + randomNumber;
+        }
+        else if(randomNumber<100){
+            return spriteName + "_0" + randomNumber;
+        }
+        else{
+            return spriteName + "_" + randomNumber;
+        }
+    }
+
+
+
+    //        GETTERS         //
 
     public String get_wall_east(){
         return this.wall_east;
